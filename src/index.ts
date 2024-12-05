c7db88f67239e8d3ebf5df969afcad921e1daba8
kynov007 <mochdickyn@gmail.com>
2024-12-05 19:16:46 +0700

    feat: Enhance API with new routes and environment configuration
    - Updated .gitignore to include generated files.
    - Modified package.json to add new dependencies for OpenAPI and Zod integration.
    - Added environment configuration using Zod for environment variable validation.
    - Implemented root and products routes with OpenAPI documentation.
    - Introduced database connection setup with Prisma.
    - Created common schemas for request parameters and response messages.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,45 @@
-import { Hono } from 'hono'
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { swaggerUI } from "@hono/swagger-ui";
+import { apiReference } from "@scalar/hono-api-reference";
 
-const app = new Hono()
+import { ProcessEnv } from "./env";
+import { rootRoute } from "./routes/root";
+import { productsRoute } from "./routes/products";
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new OpenAPIHono();
 
-export default app
+// Configure Middlewares
+app.use("*", logger()).use("*", cors());
+
+// Configure API Routes
+const apiRoutes = app
+  .basePath("/")
+  .route("/", rootRoute)
+  .route("/products", productsRoute);
+
+apiRoutes
+  .doc("/openapi.json", {
+    openapi: "3.1.0",
+    info: {
+      title: "FreshMarket API",
+      description:
+        "A modern REST API for an e-commerce fresh market application",
+      version: "v1",
+    },
+  })
+  .get("/docs/swagger", swaggerUI({ url: "/openapi.json" }))
+  .get("/docs", apiReference({ spec: { url: "/openapi.json" } }))
+  .onError((err, c) => {
+    return c.json({ code: 500, status: "error", message: err.message }, 500);
+  });
+
+console.info(`🍎 FreshMarket Backend API
+
+💽 DATABASE_URL: ${ProcessEnv.DATABASE_URL}
+`);
+
+export default app;
+
+export type ApiRoutes = typeof apiRoutes;
